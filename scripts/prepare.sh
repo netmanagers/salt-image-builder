a5ed267dcc07912eb051603de34bb5bc405a9ae9
Javier Bértoli <javier@netmanagers.com.ar>
2019-04-27 16:41:45 -0300

    Update matrix with new images, enable py3 in more distros

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,20 +3,32 @@
 echo "**** PACKER_BUILDER_TYPE is ${PACKER_BUILDER_TYPE} ****"
 echo "               **** OS is ${OS} ****"
 echo "               **** OS_VERSION is ${OS_VER} ****"
+echo "               **** PY_VERSION is ${PY_VER} ****"
 
 mkdir -p /run/sshd
 
+COMMON_PKGS="udev git net-tools"
+
 case ${OS} in
   debian|ubuntu)
-    apt-get update && apt-get install -y udev locales git procps
+    D_PKGS="${COMMON_PKGS} locales procps"
+    apt-get update && apt-get install -y ${D_PKGS}
     echo "en_US.UTF-8 UTF-8" >> /etc//etc/locale.gen
     locale-gen
     ;;
-  centos|fedora)
-    yum -y update && yum -y install udev git
+  centos)
+    if [ ${PY_VERSION} = "3" ]; then
+      C_PKGS="${COMMON_PKGS} epel-release"
+    fi
+    yum -y update && yum -y install ${C_PKGS}
+    yum -y update && yum -y install python36
+    ;;
+  fedora)
+    dnf -y update && dnf -y install ${COMMON_PKGS}
     ;;
   opensuse)
-    zypper refresh && zypper install -y udev git glibc-locale
+    O_PKGS="${COMMON_PKGS} glibc-locale net-tools-deprecated"
+    zypper refresh && zypper install -y ${O_PKGS}
     systemctl enable sshd
     ;;
 esac
