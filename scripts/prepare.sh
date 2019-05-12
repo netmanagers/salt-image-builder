982784dc74bde66520ec8cbcca311cddcf57503c
Javier Bértoli <javier@netmanagers.com.ar>
2019-05-12 19:08:26 -0300

    Add some extra packages to speed up kitchen testing

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,34 +7,39 @@ echo "               **** PY_VERSION is ${PY_VER} ****"
 
 mkdir -p /run/sshd
 
-COMMON_PKGS="udev git net-tools"
+# Adding some of the packages listed in https://github.com/test-kitchen/kitchen-docker/blob/master/lib/kitchen/driver/docker.rb
+# To speed testing
+COMMON_PKGS="udev git net-tools sudo curl"
 
 case ${OS} in
   debian|ubuntu)
-    D_PKGS="${COMMON_PKGS} locales procps"
+    D_PKGS="${COMMON_PKGS} locales procps openssh-server lsb-release"
+
     apt-get update && apt-get install -y ${D_PKGS}
     echo "en_US.UTF-8 UTF-8" >> /etc//etc/locale.gen
     locale-gen
     ;;
   centos)
+    C_PKGS="${COMMON_PKGS} openssh-server openssh-clients which"
+
     if [ "${PY_VER}" = "3" ]; then
       C_PKGS="${COMMON_PKGS} epel-release"
-    else
-      C_PKGS="${COMMON_PKGS}"
     fi
+
     yum -y update && yum -y install ${C_PKGS}
+
     if [ "${PY_VER}" = "3" ]; then
       yum -y update && yum -y install python36
     fi
-    if [ "${OS_VER}" = "6" ]; then
-      yum -y install sudo openssh-server openssh-clients which
-    fi
     ;;
   fedora)
-    dnf -y update && dnf -y install ${COMMON_PKGS}
+    F_PKGS="${COMMON_PKGS} openssh-server openssh-clients which"
+
+    dnf -y update && dnf -y install ${F_PKGS}
     ;;
   opensuse*)
-    O_PKGS="${COMMON_PKGS} glibc-locale net-tools"
+    O_PKGS="${COMMON_PKGS} glibc-locale net-tools openssh which"
+
     if [ "${OS_VER}" = "15" ]; then
       O_PKGS="${O_PKGS} net-tools-deprecated python-xml"
     fi
