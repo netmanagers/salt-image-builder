3546f8814870d61f446ca7005e5c12ea6f78f184
Javier Bértoli <javier@netmanagers.com.ar>
2019-07-10 12:10:01 -0300

    fix(tests): use --versions-report to check the correct python version is used

--- test/integration/controls/salt_pkgs.rb
+++ test/integration/controls/salt_pkgs.rb
@@ -1,10 +1,20 @@
-version = input('salt_version') == 'develop' ? 'Fluorine' : input('salt_version')
+salt_version = input('salt_version') == 'develop' ? 'Fluorine' : input('salt_version')
+python_version = input('py_version') == '3' ? '3' : '2'
 
 control 'salt call' do
   title 'should be installed'
 
   describe command('salt-call --version') do
-    its('stdout') { should match /#{version}/ }
+    its('stdout') { should match /#{salt_version}/ }
+    its('exit_status') { should eq 0 }
+  end
+end
+
+control 'salt packages' do
+  title 'should be using the desired python'
+
+  describe command('salt-call --versions-report') do
+    its('stdout') { should match /Python: #{python_version}/ }
     its('exit_status') { should eq 0 }
   end
 end
