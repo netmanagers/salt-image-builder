334fa8322184b7091acf25ea11e3bbb403ae227c
Imran Iqbal <iqbalmy@hotmail.com>
2020-10-22 14:23:45 +0100

    feat(magnesium): add `3002.0` & fix `3001` => `3001.1`
    Adjust bootstrap installation and testing for version numbers ending in
    `.0`.

--- test/integration/test_salt_version.py
+++ test/integration/test_salt_version.py
@@ -6,7 +6,9 @@ def test_salt_version(host, saltvers, installmethod):
     # v3001rc1 becomes 3001rc1
     if saltvers.startswith("v") and installmethod == "git":
         saltvers = saltvers[1:]
-    assert saltvers in cmd.stdout
+    # Handle version number when installing a `.0` release, where the `.0`
+    # is not present in the `--versions-report` output
+    assert saltvers.replace(".0", "") in cmd.stdout
     assert cmd.rc == 0
 
 
