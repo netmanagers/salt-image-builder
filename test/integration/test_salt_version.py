566c86b1d87fabf80677ea6a9067c7ab977b7cb4
Imran Iqbal <iqbalmy@hotmail.com>
2021-01-26 14:17:11 +0000

    feat(salt_version): use `SVB` to deal with version number for bootstrap
    Update test to deal with `master` & `tiamat` specifically; `latest` will
    no longer appear since that is handled by the new `SVB` variable.

--- test/integration/test_salt_version.py
+++ test/integration/test_salt_version.py
@@ -1,14 +1,23 @@
+import re
+
+
 def test_salt_version(host, saltvers, installmethod):
     cmd = host.run("salt-call --version")
-    if saltvers in ["latest", "master", "tiamat"]:
-        saltvers = "3002"
-    # Handle version branch when installing from git, remove leading "v"
-    # v3001rc1 becomes 3001rc1
-    if saltvers.startswith("v") and installmethod == "git":
-        saltvers = saltvers[1:]
-    # Handle version number when installing a `.0` release, where the `.0`
-    # is not present in the `--versions-report` output
-    assert saltvers.replace(".0", "") in cmd.stdout
+    if saltvers == "master":
+        saltvers = r"3002-n/a-\w{7}"
+    elif saltvers == "tiamat":
+        saltvers = r"3002.2|3002-\d{3}-\w{8,11}"
+    else:
+        # Handle version branch when installing from git, remove leading "v"
+        # v3001rc1 becomes 3001rc1
+        if saltvers.startswith("v") and installmethod == "git":
+            saltvers = saltvers[1:]
+        # Handle version number when installing a `.0` release, where the `.0`
+        # is not present in the `--versions-report` output
+        if saltvers.endswith(".0"):
+            saltvers = saltvers.replace(".0", "")
+    # https://github.com/pytest-dev/pytest-testinfra/issues/369#issuecomment-419763806
+    assert re.search(saltvers, cmd.stdout)
     assert cmd.rc == 0
 
 
