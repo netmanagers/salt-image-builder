2fa90698d55a0c771f10eb5b86df1eeb5a904d4a
Jamie (Bear) Murphy <jamiemurphyit@gmail.com>
2023-07-22 21:58:54 +0100

    fix(test-salt-version): version name support

--- test/integration/test_salt_version.py
+++ test/integration/test_salt_version.py
@@ -6,7 +6,7 @@ def test_salt_version(host, saltvers, installmethod):
     # See https://github.com/saltstack/salt/blob/01b22ea1b141/salt/version.py#L54-L63
     git_sha_regex = r"g?[a-f0-9]{7,40}"
     major_minor = r"3\d{3}(\.\d)?"
-    pep440_master_format = major_minor + r"\+0na." + git_sha_regex
+    pep440_master_format = major_minor + r"\+(0na|[a-z0-9]{3})." + git_sha_regex
     pep440_onedir_format = major_minor + r"(rc\d)?\+\d{1,4}." + git_sha_regex
     if saltvers == "master":
         # New PEP440-compliant `master` format
